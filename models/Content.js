24f0b1da7273ecf5101b2fe0175b45bf1511db4d
geekxing <842775263@qq.com>
2018-01-01 00:29:22 +0800

    修改数据库描述的长度

--- models/Content.js
+++ models/Content.js
@@ -2,7 +2,7 @@ const db = require('../db');
 
 const Content = db.defineAllowNull('contents', {
     keyword: { type: db.STRING(50), defaultValue: '' },
-    descr: { type: db.STRING(50), defaultValue: '' },
+    descr: { type: db.STRING(400), defaultValue: '' },
     status: { type: db.BIGINT, defaultValue: 1 }
 });
 
