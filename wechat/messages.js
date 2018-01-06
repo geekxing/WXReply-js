fd35a99ed67644e6cb9c3215501067e3ac04a366
geekxing <842775263@qq.com>
2018-01-06 15:59:03 +0800

    v2-2

--- wechat/messages.js
+++ wechat/messages.js
@@ -7,8 +7,8 @@ const db = require('../db');
 
 let systemPhrase = ['同桌','确认','修改','1','2'],
     USERINFO = 'user_info',
-    DESCRIPTION = 'descr',
-    USERNAME = 'name',
+    SEX = 'sex_input',
+    SELFDESCR = 'self_desr',
     ERROR_RESTART = '出错啦！请重新开始...';
 
 // { ToUserName: 'gh_037eaffcc126',
@@ -22,7 +22,6 @@ let systemPhrase = ['同桌','确认','修改','1','2'],
 var m = [];
 
 function STEPS_REPLY(_user, index) {
-    var keyword = _user[USERINFO];
     if (index === 0) {
         return `终于等到你
 我的江南同桌
@@ -33,38 +32,51 @@ function STEPS_REPLY(_user, index) {
 例如：王大锤+设计学院+视觉传达
 注意：一定要是真实姓名哦，不然你的同桌是找不到你的哦`;
     } else if (index === 1) {
-        return `你的关键词是：
-        
-${keyword}
+        var uinfo = _user[USERINFO];
+        return `A．	你的【姓名】+【学院】+【专业】是：
+
+${uinfo}
 
 确定 回复【1】	修改 回复【2】
 注意：一旦确定，将无法再次修改哦！`;
     } else if (index === 2) {
-        return `为什么以  ${keyword}  作为你的2018关键词呢？
-描述一下它吧~（建议50~200字）
-你详尽丰富的描述可以让Ta对你的了解更多呦~`;
+        return `你是男孩子还是女孩子呢
+我们会给你匹配一个异性同桌喔
+快来输入你的【性别】吧
+例如：男生/女生
+;`
     } else if (index === 3) {
-        return `你的【关键词描述】：
+        return `你的【性别】：
 
-${_user[DESCRIPTION]}
+${_user[SEX]}
 
 确认 回复【1】	修改 回复【2】`;
     } else if (index === 4) {
-        return `输入你的【姓名】+【学院】+【专业】吧~
-注意：一定是真实姓名，对方才可以找到你哦~`;
+        return `好啦，你的新同桌快要到你面前啦
+做一个简单的【自我介绍】吧
+（建议20~200字）系统限制10-800字
+让你的同桌可以更加了解你
+你详尽丰富的自我介绍可以让Ta对你的了解更多呦~`;
     } else if (index === 5) {
-        return `你的名字、学院及专业：
-        
-${_user[USERNAME]}
+        return `你的自我介绍
+
+${_user[SELFDESCR]}
 
 确认 回复【1】 修改 回复【2】`;
     } else if (index === 6) {
-        return `车票已经预定成功，系统正在为你匹配旅伴中，请耐心等候Ta的出现哦！
-为了确保所有的参与同学均是江南大学学生，只有使用校上行APP，才可以找到彼此哦！
-【校上行】是由江南大学在校生自主开发的一款社交APP，目前只对江南大学用户开放哦！在这里，你可以结识全校的任何一位同学，「校友通讯录」中精细分类学院、专业、年级，让你校内找人从此不再是难事！你也可以加入「壹周约行」，和校友一起约吃饭运动看电影打游戏……打破你有限的人际圈，结识更多新同学~先戳这里http://a.app.qq.com/o/simple.jsp?pkgname=com.xiaoshangxing 
-下载【校上行】，在校上行APP里等待那个和你关键词一样的旅伴，开启你们的18号巴士之旅吧！
-你也可以先回复“看关键词” 看看别人的关键词。系统将随机为你发送五个他人的关键词，你可以选择一个你感兴趣的同学，去校上行跟他say hi哦~`;
+        return `正在为你匹配你的江南同桌，你会在24小时内收到你的同桌信息哦！
+这是一个只有江南大学的学生才可以参加的活动，为了确保所有的参与同学均是江南大学学生，只有使用校上行APP，才可以找到彼此哦！
+【校上行】是由江南大学在校生自主开发的一款社交APP！在这里，你可以结识全校的任何一位同学，「校友通讯录」中精细分类学院、专业、年级，让你校内找人从此不再是难事！你也可以加入「壹周约行」，和校友一起约吃饭运动看电影打游戏……打破你有限的人际圈，结识更多新同学~先戳这里http://a.app.qq.com/o/simple.jsp?pkgname=com.xiaoshangxing 
+下载【校上行】，在校上行APP里等待你的江南同桌出现吧！`;
     }
+}
+
+function propNameByStep(l) {
+    var prop = '';
+    if (l === 2) prop = USERINFO;
+    else if (l === 4) prop = SEX;
+    else prop = SELFDESCR;
+    return prop;
 }
 
 function replyAdd(reply, user) {
@@ -109,26 +121,14 @@ async function updateUserContent(_user, k) {
     var from = _user['id'];
     var user = await User.findOne({where:{fromUserName:from}});
     if (!user) {
-        user = await User.create({fromUserName:from});
-        user[k] = value;
+        var info = await getUser(from);
+        info['fromUserName'] = from;
+        info[k] = value;
+        user = await User.create(info);
     } else  {
         user[k] = value;
     }
     await user.save();
-}
-
-/* 洗牌算法 */
-Array.prototype.shuffle = function () {
-    var arr = this;
-    var input = [];
-
-    for (var i = 0, len = arr.length; i < len; i++) {
-        var j = Math.floor(Math.random() * arr.length);
-        input[i] = this[j];
-        arr.splice(j, 1);
-    }
-    console.log(input);
-    return input;
 }
 
 function replyMessage() {
@@ -159,7 +159,6 @@ function replyMessage() {
             return '';
         }
         var content = message.Content,
-            isInLength = (content.length > 0 && content.length < 4),
             isNotSystemPhrase = systemPhrase.indexOf(content) === -1,
             index = l - 1,
             reply = STEPS_REPLY(_user, index);
@@ -171,10 +170,7 @@ function replyMessage() {
             return '';
         } else if (content.length>0 && isNotSystemPhrase && (l === 2 || l === 4 || l === 6)) {
             //关键词长度不超过3个字，且不能是系统词汇
-            var prop = '';
-            if (l === 2) prop = USERINFO;
-            else if (l === 4) prop = DESCRIPTION;
-            else prop = USERNAME;
+            var prop = propNameByStep(l);
             _user[prop] = content;
             reply = STEPS_REPLY(_user, index);
             return replyAdd(reply, _user);
@@ -182,24 +178,13 @@ function replyMessage() {
             if (content === '2') {
                 //修改
                 var field = '';
-                if (l === 3) field = '【关键词】';
-                else if (l === 5) field = '【关键词描述】';
-                else  field = '【姓名】+【学院与专业】';
+                if (l === 3) field = '【姓名】+【学院】+【专业】';
+                else if (l === 5) field = '【性别】';
+                else  field = '【自我介绍】';
                 return replyMinus(`重新输入你的${field}吧`, _user);
             } else if (content === '1') {
-                if (l === 3) {  //存储关键词
-                    await updateUserContent(_user, USERINFO);
-                } else if (l === 5) { //存储关键词描述
-                    var rep = await updateUserContent(_user, DESCRIPTION);
-                    if (rep !== undefined && rep.length > 0) {
-                        m.splice(fid, 1);
-                        return rep;
-                    }
-                } else { //存储用户姓名
-                    var user = await User.findOne({where: {fromUserName: from}});
-                    user.user_name = _user[USERNAME];
-                    await user.save();
-                }
+                var prop = propNameByStep(l-1);
+                await updateUserContent(_user, prop);
                 console.log('开始下一个描述');
                 return replyAdd(reply, _user);
             } else {
