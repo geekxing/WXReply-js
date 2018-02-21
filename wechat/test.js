eb589d809c57bddb9c1cf5bb1321a0f31c2d689e
geekxing <842775263@qq.com>
2018-02-21 19:12:03 +0800

    v3

---
+++ wechat/test.js
@@ -0,0 +1,16 @@
+const API = require('co-wechat-api');
+const config = require('../config');
+
+async function uploadVoice(filepath) {
+    console.log(`上传语音... `);
+    let api = new API(config.wechat.appid, config.wechat.appSecret);
+    try {
+        let result = await api.uploadMaterial(filepath, 'voice');
+        console.log(result);
+        return result;
+    } catch (e) {
+        throw new Error(e);
+    }
+}
+
+uploadVoice(`/wechat/mediaLib/xiaoshangxing.mp3`);
