34be15a55b33c26bb8fd57b4ca7019678e60361e
geekxing <842775263@qq.com>
2018-03-04 13:57:43 +0800

    v4- rose final

--- wechat/messages.js
+++ wechat/messages.js
@@ -4,12 +4,10 @@ function replyMessage() {
     return async (message, ctx) => {
         console.log(message);
         // 微信输入信息就是这个 message
-        let from = message.FromUserName;
         if (message.MsgType === 'text' && message.Content === undefined) {
             return '';
         }
-        let content = message.MsgType === 'text' ? message.Content : '',
-            index = l - 1;
+        let content = message.MsgType === 'text' ? message.Content : '';
         if (content === `玫瑰行动`) {
             return `想知道「玫瑰行动」的暗号吗？
 偷偷告诉你，可不要告诉别人哦！
